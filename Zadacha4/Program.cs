91c62ba5e264b02e0cc90f9861054051f8dcd0a6
Nina <you@example.com>
2022-12-16 20:52:31 +0300

    Добавлена 4 задача

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int N = 1;
+
+while(N <= num)
+{
+   if(N % 2 == 0)
+    {
+    Console.WriteLine($"Числа {N}");
+    N++;
+    }
+    else
+    {
+        N++;
+    } 
+}
+
+   
+    
+
+ 
+
